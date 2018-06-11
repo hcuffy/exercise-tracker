3785bc986df062af6c0f88c1718d9b5f7e75be9a
Henry Cuffy <hcuffy_5@hotmail.com>
2018-06-11 17:17:44 +0200

    Setup server

--- server.js
+++ server.js
@@ -1,8 +1,23 @@
-const path = require('path');
 const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
+const routes =  require('./routes');
 
 const app = express();
 
 mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/voting');
+
+
+app.use(express.static('public'));
+app.use('/', routes);
+
+app.get("/", function (request, response) {
+  response.sendFile(__dirname + '/views/index.html');
+});
+
+const port = process.env.PORT || 3000;
+
+app.listen(port, () => {
+console.log('Express server has started on', port);
+
+});
